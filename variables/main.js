97e93b126ce509dc3d6e9853cfdf976f2bd9d227
daviddaren <daviddarenit@gmail.com>
2022-10-30 15:13:40 +0200

    first commit

---
+++ variables/main.js
@@ -0,0 +1,17 @@
+// string
+const str = "";
+const str1 = "";
+const str2 = ``;
+// undefined
+let pivozavr;
+// null
+// number
+let num1 = 1;
+let num2 = 12.23;
+// bollean
+// true
+// false
+// sloznije tipi
+// function
+// array
+// oboject
